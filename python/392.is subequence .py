0bce2850eadaa6db1871db745cea486050ac1764
Samir Omer <samiromer2@yahoo.com>
2026-04-15 20:50:08 -0400

    list 2 day 1

---
+++ python/392.is subequence .py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0  # pointer for s
+        
+        for j in range(len(t)):
+            if i < len(s) and s[i] == t[j]:
+                i += 1
+        
+        return i == len(s)
